de186f280adc55ef23949a82978b994f2dfc186c
unknown <techsd9@gmail.com>
2020-12-03 04:48:10 -0800

    Components part2

--- src/App.js
+++ src/App.js
@@ -9,16 +9,27 @@ import { DISHES } from './shared/dishes';
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+   
+    this.state={
+      dishes: DISHES 
+    };
+
+  }
+
+
   render() {
 
     return (
-      <div className="App">
+      <div>
           <Navbar dark color="primary">
             <div className="Container">
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
           </Navbar>
-          <Menu />
+          <Menu dishes={this.state.dishes} />
       </div>
 
     );
