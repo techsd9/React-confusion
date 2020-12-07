d4a18e9ddf124c0d15ff1cbe6091e9d5180b2b90
unknown <techsd9@gmail.com>
2020-12-07 05:42:19 -0800

    Front-end Web Development with React, Week-1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Dishdetail extends Component
+{
+ 	constructor(props){
+		super(props);
+	
+	this.state= {
+		selectedDish: null
+		};
+	}
+
+
+
+	renderDish(selectedDish){
+		if (selectedDish!= null) {
+			return(
+				<div key={selectedDish.id} className="col-12 col-md-5 m-1">
+				<Card>
+					<CardImg width="100%" src={selectedDish.image} alt={selectedDish.name} />
+					<CardBody>
+						<CardTitle>{selectedDish.name}</CardTitle>
+						<CardText>{selectedDish.description}</CardText>
+					</CardBody>
+				</Card>
+				</div>
+				)
+		}
+		else {
+			<div></div>
+		}
+	}
+
+	renderComments(comments){
+		// console.log(comments);
+
+		if (comments!=null) {
+
+			const listItems= comments.map((c)=> {
+				return(
+					<div key={c.id}>
+						<ul className= "list-unstyled">
+							<li>{c.comment}</li>
+							<li>-- {c.author}</li>
+							<li>{c.date}</li>
+						</ul>
+					</div>
+				)
+			});
+
+
+			return (
+				<div className="col-12 col-md-5 m-1">
+				<Card>
+					<CardBody>
+						<h4>Comments</h4>
+						{listItems}
+					</CardBody>
+				</Card>
+				</div>
+			);
+
+		}else {
+			<div></div>
+		}
+	}
+
+
+
+	render(){	
+		return (
+				<div className="container">
+					<div className="row">
+						{this.renderDish(this.props.selectedDish)} 
+					    {this.renderComments(this.props.comments)} 
+					</div>
+				</div>
+
+				);			
+		}	
+}
+
+export default Dishdetail;
+
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,65 +1,60 @@
 import React, { Component } from 'react';
 import { Media } from 'reactstrap';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
 
-
-class Menu extends Component{
-	constructor(props) {
+class Menu extends Component
+	{
+	constructor(props) 
+		{
 		super(props);
-
 		this.state = {
-			selectedDish: null
-		}
+			selectedDish: null,
+			comments: null
+		};
 	}
 
-	onDishSelect(dish){
-		this.setState({ selectedDish: dish});
+
+	onDishSelect(dish){ 	
+		this.setState({ selectedDish: dish })
 	}
 
-	renderDish(dish){
-		if (dish!= null) {
-			return(
-				<Card>
-					<CardImg width="100%" src={dish.image} alt={dish.name} />
-					<CardBody>
-						<CardTitle>{dish.name}</CardTitle>
-						<CardText>{dish.description}</CardText>
-					</CardBody>
-				</Card>
-
-				)
-		}
-		else {
-			<div></div>
-		}
+	getSelectedDishComments(selectedDish){
+		this.setState({ comments: selectedDish.comments});
 	}
+
 
 	render(){
-		const menu= this.props.dishes.map((dish)=> {
+		const menu= this.props.dishes.map((dish)=> 
+		{
 			return(
 				<div key={dish.id} className="col-12 col-md-5 m-1">
-					<Card onClick={()=> this.onDishSelect(dish)}>
-						<CardImg width="100%" src={dish.image} alt={dish.name} />
-						<CardImgOverlay>
-							<CardTitle>{dish.name}</CardTitle>
-						</CardImgOverlay>	
+					<Card onClick ={()=> {this.onDishSelect(dish); this.getSelectedDishComments(dish);}}>
+							<CardImg width="100%" src={dish.image} alt={dish.name} />
+							<CardImgOverlay>
+								<CardTitle>{dish.name}</CardTitle>
+							</CardImgOverlay>	
 					</Card>
 				</div>
 			);
 		});
 
 
+
+
 		return (
 			<div className="container">
 				<div className="row">
-						{menu}
+					{menu}
 				</div>
 				<div className="row">
-					{this.renderDish(this.state.selectedDish)}
+					<Dishdetail selectedDish={this.state.selectedDish} 
+					comments={this.state.comments}
+                   	 />
 				</div>
 			</div>
-
 			);
 	}
 }
 export default Menu; 
+
